9941c73129beabcaea31cf747bc8cebe1311d4c9
sorozcov <42949349+sorozcov@users.noreply.github.com>
2019-02-15 11:05:35 -0600

    inicio

---
+++ app/src/main/java/com/example/musica/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.musica
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Crear un link entre el listview y el adapter
+        //val adapter = ArrayAdapter<Contact>(this, R.layout.listviews, ContactApplication.contacts)
+        //allContacts.adapter = adapter
+    }
+}
